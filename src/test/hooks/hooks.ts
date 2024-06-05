7f1385356c04d3ce20ba86418b4240f79e123a15
sumitgithub007 <goyalsumit319@gmail.com>
2024-06-05 10:44:49 +0530

    added playwright yml file

--- src/test/hooks/hooks.ts
+++ src/test/hooks/hooks.ts
@@ -1,13 +1,12 @@
 import { BeforeAll,AfterAll, After,Before, Status, AfterStep } from "@cucumber/cucumber";
-import { Browser, BrowserContext, chromium ,Page} from "@playwright/test";
+import { Browser, BrowserContext} from "@playwright/test";
 import { fixture} from "./pageFixture";
- 
-
-import { time } from "console";
 import { invokeBrowser } from "../../helper/browsers/browserManager";
 import { getEnv } from "../../helper/env/env";
 import { createLogger } from "winston";
 import { options } from "../../helper/utils/logger";
+const fs= require("fs-extra");
+
 let browser:Browser;
 let context:BrowserContext;
 let timestamp:string
@@ -22,10 +21,8 @@ BeforeAll(async function () {
 Before(async function ({pickle}) { 
 
  
-    context = await browser.newContext({viewport: {
-        width: 1280,
-        height: 674,
-    },
+    context = await browser.
+    newContext({viewport: { width: 1280,height: 674},recordVideo:{dir:"test-results/video"}
     
     });
 
@@ -39,31 +36,42 @@ Before(async function ({pickle}) {
  
 After(async function ({pickle,result}) { 
 
+    let videoPath: string;
+    videoPath = await fixture.page.video().path();
+    
    // const timestamp = new Date().toISOString().replace(/[-:]/g, ''); // Generate timestamp
     //const filename = `./test-result/screenshots/${pickle.id}_${timestamp}.png`; // Concatenate timestamp with filename
-
-    //const img = await pageFixture.page.screenshot({ path: filename, type: "png" });
+     //const img = await pageFixture.page.screenshot({ path: filename, type: "png" });
  
    //const img = await pageFixture.page.screenshot({path:`./test-result_${timestamp}/screenshots/${pickle.id}.png`,type:"png"})
   console.log(result?.status);
-  if(result?.status==Status.FAILED)
-    {
-        const img = await fixture.page.screenshot({path:`./test-result/screenshots/${pickle.id}.png`,type:"png"})
-        await this.attach(img,"image/png"); 
-    } 
-
-   await fixture.page.close();
+  
+//   if(result?.status==Status.FAILED)
+//     {
+//         const img = await fixture.page.screenshot({path:`./test-result/screenshots/${pickle.id}.png`,type:"png"})
+//         await this.attach(img,"image/png"); 
+//     } 
+     
+const img = await fixture.page.screenshot();
+    await fixture.page.close();
     await context.close();
-   
+    if (result?.status == Status.PASSED) {
+        
+        await this.attach(
+            fs.readFileSync(videoPath),
+            'video/webm'
+        )};
 
+      
+       await this.attach(img,"image/png"); 
 })
 
 //Below is working and correct
  AfterStep(async function ({pickle,result}) {
     
     // timestamp = String(Date.now())//;
-     const img = await fixture.page.screenshot();
-     await this.attach(img,"image/png"); 
+    //  const img = await fixture.page.screenshot();
+    //  await this.attach(img,"image/png"); 
  })
 
 AfterAll(async function () { 
